031c3277f1c6c4b9991b332b17dc909be990f453
chao3.xu <chao3.xu@ly.com>
2024-01-09 13:21:48 +0800

    init:祈聆

---
+++ lib/data/category_database.dart
@@ -0,0 +1,180 @@
+/// Category DataBase
+import 'package:flutter/material.dart';
+
+List<_CategoryData> database = [
+  _CategoryData(
+    categoryIcon: Icon(IconData(0xe80a, fontFamily: 'CategoryIcons')),
+    categoryTitle: '雨声',
+    categoryTag: '沉浸在雨中',
+    categoryInfo:
+        '一组来自自然界的白噪声，包含一些音频文件。',
+    imagePath: 'assets/images/cover_rain.jpg',
+    audioList: [
+      _AudioData(
+        audioTitle: '撕裂我的心',
+        audioLength: 72,
+        audioImagePath: 'assets/images/rain_01.jpg',
+      ),
+      _AudioData(
+        audioTitle: '昆虫的叫声',
+        audioLength: 104,
+        audioImagePath: 'assets/images/rain_02.jpg',
+      ),
+      _AudioData(
+        audioTitle: '喜悦的泪水',
+        audioLength: 72,
+        audioImagePath: 'assets/images/rain_03.jpg',
+      ),
+    ],
+  ),
+  _CategoryData(
+    categoryIcon: Icon(IconData(0xe806, fontFamily: 'CategoryIcons')),
+    categoryTitle: '森林',
+    categoryTag: '身处神奇的森林',
+    categoryInfo:
+        '一组来自自然界的白噪声，包含四个音频文件。',
+    imagePath: 'assets/images/cover_forest.jpg',
+    audioList: [
+      _AudioData(
+        audioTitle: '风声',
+        audioLength: 72,
+        audioImagePath: 'assets/images/forest_01.jpg',
+      ),
+      _AudioData(
+        audioTitle: '昆虫的叫声',
+        audioLength: 104,
+        audioImagePath: 'assets/images/forest_02.jpg',
+      ),
+      _AudioData(
+        audioTitle: '鸟的旋律',
+        audioLength: 72,
+        audioImagePath: 'assets/images/forest_03.jpg',
+      ),
+      _AudioData(
+        audioTitle: '鸟的旋律',
+        audioLength: 72,
+        audioImagePath: 'assets/images/forest_04.jpg',
+      ),
+    ],
+  ),
+  _CategoryData(
+    categoryIcon: Icon(IconData(0xe808, fontFamily: 'CategoryIcons')),
+    categoryTitle: '大自然',
+    categoryTag: '母亲的存在之美',
+    categoryInfo:
+        '一组来自自然界的白噪声，包含一些音频文件。',
+    imagePath: 'assets/images/cover_natural.jpg',
+    audioList: [
+      _AudioData(
+        audioTitle: '沉默的石头',
+        audioLength: 72,
+        audioImagePath: 'assets/images/natural_01.jpg',
+      ),
+      _AudioData(
+        audioTitle: '孤独的母亲',
+        audioLength: 104,
+        audioImagePath: 'assets/images/natural_02.jpg',
+      ),
+      _AudioData(
+        audioTitle: '像极光一样闪耀',
+        audioLength: 72,
+        audioImagePath: 'assets/images/natural_03.jpg',
+      ),
+      _AudioData(
+        audioTitle: '冷静心情',
+        audioLength: 104,
+        audioImagePath: 'assets/images/natural_04.jpg',
+      ),
+      _AudioData(
+        audioTitle: '洞穴呼唤',
+        audioLength: 72,
+        audioImagePath: 'assets/images/natural_05.jpg',
+      ),
+    ],
+  ),
+  _CategoryData(
+    categoryIcon: Icon(IconData(0xe805, fontFamily: 'CategoryIcons')),
+    categoryTitle: '节奏',
+    categoryTag: '最大节奏',
+    categoryInfo:
+        '一组来自自然界的白噪声，包含一些音频文件。',
+    imagePath: 'assets/images/cover_flow.jpg',
+    audioList: [
+      _AudioData(
+        audioTitle: '水声',
+        audioLength: 72,
+        audioImagePath: 'assets/images/flow_01.jpg',
+      ),
+      _AudioData(
+        audioTitle: '跟随这个节奏',
+        audioLength: 104,
+        audioImagePath: 'assets/images/flow_02.jpg',
+      ),
+      _AudioData(
+        audioTitle: '融化我的心',
+        audioLength: 72,
+        audioImagePath: 'assets/images/flow_03.jpg',
+      ),
+      _AudioData(
+        audioTitle: '鹅卵石的声音',
+        audioLength: 72,
+        audioImagePath: 'assets/images/flow_04.jpg',
+      ),
+    ],
+  ),
+  _CategoryData(
+    categoryIcon: Icon(IconData(0xe809, fontFamily: 'CategoryIcons')),
+    categoryTitle: '其他',
+    categoryTag: '正如你的心所说',
+    categoryInfo:
+        '一组来自自然界的白噪声，包含一些音频文件。',
+    imagePath: 'assets/images/cover_other.jpg',
+    audioList: [
+      _AudioData(
+        audioTitle: '风声',
+        audioLength: 72,
+        audioImagePath: 'assets/images/other_01.jpg',
+      ),
+      _AudioData(
+        audioTitle: '昆虫的叫声',
+        audioLength: 104,
+        audioImagePath: 'assets/images/other_02.jpg',
+      ),
+      _AudioData(
+        audioTitle: '鸟的旋律',
+        audioLength: 72,
+        audioImagePath: 'assets/images/other_03.jpg',
+      ),
+    ],
+  )
+];
+
+class _CategoryData {
+  final String categoryTitle;
+  final String categoryTag;
+  final String imagePath;
+  final String categoryInfo;
+  final List<_AudioData> audioList;
+  final Icon categoryIcon;
+
+  _CategoryData({
+    required this.categoryTitle,
+    required this.categoryTag,
+    required this.imagePath,
+    required this.categoryInfo,
+    required this.audioList,
+    required this.categoryIcon,
+  });
+}
+
+class _AudioData {
+  final String audioTitle;
+  final int audioLength;
+  final String audioImagePath;
+
+  _AudioData({
+    required this.audioTitle,
+    required this.audioLength,
+    required this.audioImagePath,
+  });
+}
---
+++ lib/main.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'bloc/active_index_bloc.dart';
+import 'components/category_list.dart';
+import 'components/menu_icon.dart';
+import 'components/player_controls.dart';
+import 'components/selected_category_section.dart';
+import 'components/title_widget.dart';
+import 'components/user_card.dart';
+import 'data/category_database.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.grey[50],
+    statusBarIconBrightness: Brightness.dark,
+    systemNavigationBarColor: Colors.grey[50],
+    systemNavigationBarIconBrightness: Brightness.dark,
+  ));
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  runApp(MyApp());
+}
+
+// Global Variables
+double lExpanded = 0;
+double rExpanded = 0;
+double lCollapsed = 0;
+double rCollapsed = 0;
+double lMidExpanded = 0;
+double lMidCollapsed = 0;
+double rMidExpanded = 0;
+double rMidCollapsed = 0;
+Duration normal = Duration(milliseconds: 300);
+Duration fast = Duration(milliseconds: 200);
+Duration superFast = Duration(milliseconds: 100);
+bool leftActive = true;
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      // HomePage is the main page.
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  // Controller for Rotation Animation.
+  late Animation<double> rotateAnim;
+  late AnimationController rotationController;
+
+  @override
+  void initState() {
+    super.initState();
+    leftActive = true;
+
+    rotationController = AnimationController(vsync: this, duration: superFast);
+    rotateAnim =
+        Tween<double>(begin: 0, end: 0.125).animate(rotationController);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    lExpanded = 4 * MediaQuery.of(context).size.width / 5 - 0.5;
+    lCollapsed = MediaQuery.of(context).size.width / 3 - 0.5;
+    rExpanded = 2 * MediaQuery.of(context).size.width / 3 - 0.5;
+    rCollapsed = MediaQuery.of(context).size.width / 5 - 0.5;
+    lMidExpanded = MediaQuery.of(context).size.height - 2 * rCollapsed - 24;
+    lMidCollapsed = MediaQuery.of(context).size.height - lExpanded - 24;
+    rMidExpanded = MediaQuery.of(context).size.height - 2 * rCollapsed - 24;
+    rMidCollapsed =
+        MediaQuery.of(context).size.height - rCollapsed - lExpanded - 24;
+  }
+
+  void expandLeftSection() {
+    // This function expands left section.
+    setState(() {
+      leftActive = true;
+      leftActive ? rotationController.reverse() : rotationController.forward();
+    });
+  }
+
+  void expandRightSection() {
+    // This function expands right section.
+    setState(() {
+      leftActive = false;
+      leftActive ? rotationController.reverse() : rotationController.forward();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: StreamBuilder(
+        // This gets the active Category's index number.
+        initialData: 0,
+        stream: activeIndexBloc.streamActiveIndex,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          return Container(
+            // Base Widget.
+            padding: EdgeInsets.only(top: 24),
+            child: Row(
+              // This Row separtaes the Left Section from the one on the Right Side.
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {},
+                  child: AnimatedContainer(
+                    //Left Section
+                    duration: normal,
+                    color: Colors.transparent,
+                    width: leftActive ? lExpanded : lCollapsed,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        // Top Left Widget: App Title
+                        titleWidget(),
+
+                        // Mid Left Widget: Category Info & Contents
+                        selectedCategorySection(
+                          activeIndex: snapshot.data,
+                          totalItems: database[snapshot.data].audioList.length,
+                          categoryTag: database[snapshot.data].categoryTag,
+                          imagePath: database[snapshot.data].imagePath,
+                          categoryTitle: database[snapshot.data].categoryTitle,
+                          trigger: expandLeftSection,
+                        ),
+
+                        // Bottom Left Widget: Player Controls
+                        PlayerControls(),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  //Seperator
+                  color: Colors.grey[200],
+                  width: 1,
+                ),
+                AnimatedContainer(
+                  //Right Section
+                  duration: normal,
+                  color: Colors.transparent,
+                  width: leftActive ? rCollapsed : rExpanded,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      // Top Right Widget: Rotating Menu Icon
+                      menuIcon(
+                        rotateAnim: rotateAnim,
+                        rotationController: rotationController,
+                        trigger: expandRightSection,
+                      ),
+
+                      // Middle Right Widget: List of all Categories
+                      categoryList(activeIndex: snapshot.data),
+
+                      // Bottom Right Widget: User Info Widget
+                      userCard(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
