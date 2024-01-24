702802826d8490e08b6a637b49a992fa3d0cd41d
chao3.xu <chao3.xu@ly.com>
2024-01-24 14:48:51 +0800

    pref:webViewController加载本地html

--- lib/page/web/web_new_year_controller.dart
+++ lib/page/web/web_new_year_controller.dart
@@ -1,15 +1,27 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class NewYearWebLoginController {
   InAppWebViewController? webViewController;
   final CookieManager cookieManager = CookieManager.instance();
-  void onWebViewCreated(InAppWebViewController controller) {
+  Future<void> onWebViewCreated(InAppWebViewController controller) async {
     webViewController = controller;
-    webViewController!.loadUrl(
-      urlRequest: URLRequest(
-        url: Uri.parse("http://150.158.103.146/qiling/NewYear/"),
-      ),
-    );
+    webViewController!.loadFile(assetFilePath: "assets/html/flame/index.html");
+    // webViewController!.loadUrl(
+    //   urlRequest: URLRequest(
+    //     url: Uri.parse("http://150.158.103.146/qiling/NewYear/"),
+    //     // url: Uri.file(getAssetsPath("assets/html/flame/index.html")),
+    //   ),
+    // );
+  }
+
+  String getAssetsPath(String path) {
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      return 'file:///android_asset/flutter_assets/' + path;
+    } else {
+      return 'file://Frameworks/App.framework/flutter_assets/' + path;
+    }
   }
 
   void onLoadStop(InAppWebViewController controller, Uri? uri) async {
