c89c516366f86c96808bb769729368f7616a067a
chao3.xu <chao3.xu@ly.com>
2024-02-02 10:32:37 +0800

    pref:限制macos app的屏幕尺寸

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -2,14 +2,17 @@ import Cocoa
 import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
-  override func awakeFromNib() {
-    let flutterViewController = FlutterViewController.init()
-    let windowFrame = self.frame
-    self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+    override func awakeFromNib() {
+      let flutterViewController = FlutterViewController.init()
+      let windowFrame = self.frame
+      self.contentViewController = flutterViewController
+      self.setFrame(windowFrame, display: true)
+      self.setContentSize(NSSize(width: 1284,height: 2778))
+      let window: NSWindow! = self.contentView?.window
+      window.styleMask.remove(.resizable)
 
-    RegisterGeneratedPlugins(registry: flutterViewController)
+      RegisterGeneratedPlugins(registry: flutterViewController)
 
-    super.awakeFromNib()
-  }
+      super.awakeFromNib()
+    }
 }
